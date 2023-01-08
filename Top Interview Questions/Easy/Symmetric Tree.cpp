9946b65f0c06129fdae99718916f8cf1815c5afb
Sai Mahadev <mahadevvone@gmail.com>
2023-01-08 22:27:18 +0530

    Symmetric Tree LeetCode Solution

---
+++ Top Interview Questions/Easy/Symmetric Tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool isInSync(TreeNode *left, TreeNode *right) {
+        if(left == nullptr && right == nullptr) return true;
+        if(left == nullptr || right == nullptr) return false;
+        return (left->val == right->val && isInSync(left->left, right->right) && isInSync(left->right, right->left));
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(root == nullptr) return true;
+        return isInSync(root->left, root->right);
+    }
+};
